57eded89238ca5422581f92ae35b573dd36054d7
supriya-mallick <supriyamallick132@gmail.com>
2022-01-24 09:08:16 -0800

    First Commit

---
+++ selenium2/src/test/java/sample1/Calculator.java
@@ -0,0 +1,18 @@
+package sample1;
+
+public class Calculator {
+
+		public int add(int a,int b) {
+			return a+b;
+		}
+		public int sub(int a,int b) {
+			return a-b;
+		}
+		public int multiply(int a,int b) {
+			return a*b;
+		}
+			public int sub1(int a,int b) {
+				return a/b;
+			}
+
+}
